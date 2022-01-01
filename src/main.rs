de2c5f21c17445c0a126c10f712cc8ceedbd62b4
joao Felipe Santoro Pinto <joao.santoro.pinto@concrete.com.br>
2022-01-01 14:34:25 -0300

    Solving problem 002

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ pub mod problems;
 fn main() {
     println!("let's solve!");
     println!("problem 001 solution is: {}", problems::problem001::solve(1000));
+    println!("problem 002 solution is: {}", problems::problem002::solve(33));
 
 }
